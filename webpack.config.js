2d8c5e6f7425d091b409ab9563d69404dd02db7b
Jean LETHIEC <jean.lethiec@carbon-it.com>
2019-10-18 14:24:25 +0200

    Fixed an error in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,6 @@ module.exports = {
     }),
   ],
   resolve: {
-    modules: [path.resolve(__dirname, './src'), , 'node_modules'],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
   },
 };
